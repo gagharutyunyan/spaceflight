4f0f09e91340ef1de1a99437c4622794e3561ec6
gagharutyunyan <gagharut@gmail.com>
2021-04-04 17:14:47 +0300

    fix problem with next launches

--- src/api/boardRoutesData.ts
+++ src/api/boardRoutesData.ts
@@ -21,7 +21,6 @@ export const routes = [
 
 export const redirectingRoutes = [
   { id: 1, path: '/', exact: true, redirectTo: '/board/past' },
-  { id: 2, path: '/board/*', exact: true, redirectTo: '/board/past' },
   { id: 3, path: '/board/past/*', exact: true, redirectTo: '/board/past' },
   { id: 4, path: '/board/next/*', exact: true, redirectTo: '/board/next' },
 ];
